2fabce8cda2432b11cf20f88b604a3299b12ff8e
chenjianzhao <j.623487211@163.com>
2018-04-08 00:58:17 +0800

    learn spring cloud

--- service-ribbon/src/main/java/com/example/demo/controller/HelloController.java
+++ service-ribbon/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
